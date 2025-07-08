0dfc3a9425cd09fb73cb6aa516c486c3f1115a95
Vlad <vladswork95@gmail.com>
2025-07-08 11:00:14 +0100

    add words list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
+import words from "./assets/wordList.json";
 
 export default function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  console.log(wordToGuess);
   return <h1>Hi</h1>;
 }
