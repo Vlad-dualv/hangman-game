cf8c4c986500585aae658e3be3a1412d14289c06
Vlad <vladswork95@gmail.com>
2025-07-08 11:32:17 +0100

    style: hang device layout

---
+++ src/components/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import words from "../assets/wordList.json";
+import { useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
+
+export default function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string>([]);
+
+  console.log(wordToGuess);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
+}
